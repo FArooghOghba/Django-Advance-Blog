c4e38533678a0e6effacd48d21109d1afa950da0
FArooghOghba <faroogh.oghba@gmail.com>
2022-11-22 16:37:48 +0330

    refactor: rewriting post detail view with RetrieveUpdateDestroyAPIView

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, get_list_or_404
 
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
@@ -19,50 +19,15 @@ class PostListView(ListCreateAPIView):
     queryset = Post.objects.all()
 
 
-class PostDetailView(ListCreateAPIView):
+class PostDetailView(RetrieveUpdateDestroyAPIView):
     """
     getting detail of a post, edit and delete the post.
     """
 
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
-
-    def get(self, request, post_id):
-        """
-        retrieving an object of a post class.
-        :param request:
-        :param post_id:
-        :return:
-        """
-        post = get_object_or_404(Post, pk=post_id)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-
-    def put(self, request, post_id):
-        """
-        editing an object of a post class.
-        :param request:
-        :param post_id:
-        :return:
-        """
-
-        post = get_object_or_404(Post, pk=post_id)
-        serializer = self.serializer_class(post, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, post_id):
-        """
-        deleting an object of a post class.
-        :param request:
-        :param post_id:
-        :return:
-        """
-
-        post = get_object_or_404(Post, pk=post_id)
-        post.delete()
-        return Response({'detail': 'item removed successfully.'}, status=HTTP_204_NO_CONTENT)
+    queryset = Post.objects.all()
+    lookup_url_kwarg = 'post_id'
 
 
 # Post List View in Class Based View with APIView.
@@ -140,3 +105,56 @@ def post_detail_view(request, post_id):
         post.delete()
         return Response({'detail': 'item removed successfully.'}, status=HTTP_204_NO_CONTENT)
 """
+
+
+# Post Detail View in Class Based View with APIView.
+
+'''
+from rest_framework.views import APIView
+
+class PostDetailView(ListCreateAPIView):
+    """
+    getting detail of a post, edit and delete the post.
+    """
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get(self, request, post_id):
+        """
+        retrieving an object of a post class.
+        :param request:
+        :param post_id:
+        :return:
+        """
+        post = get_object_or_404(Post, pk=post_id)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+
+    def put(self, request, post_id):
+        """
+        editing an object of a post class.
+        :param request:
+        :param post_id:
+        :return:
+        """
+
+        post = get_object_or_404(Post, pk=post_id)
+        serializer = self.serializer_class(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, post_id):
+        """
+        deleting an object of a post class.
+        :param request:
+        :param post_id:
+        :return:
+        """
+
+        post = get_object_or_404(Post, pk=post_id)
+        post.delete()
+        return Response({'detail': 'item removed successfully.'}, status=HTTP_204_NO_CONTENT)
+
+'''
