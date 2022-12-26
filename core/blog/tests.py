90bd99bcf97c7c56f0cf7ccec4f04127e406ce35
FArooghOghba <faroogh.oghba@gmail.com>
2022-12-26 20:37:44 +0330

    built: test for blog app

--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .views import (
+    IndexTemplateView, PostListView, PostDetailView
+)
 
 # Create your tests here.
+
+class TestUrl(TestCase):
+
+    def test_blog_index_url_resolver(self):
+        url = reverse('blog:index')
+        self.assertEquals(
+            resolve(url).func.view_class,
+            IndexTemplateView
+        )
+
+    def test_blog_post_list_url_resolver(self):
+        url = reverse('blog:post-list')
+        self.assertEquals(
+            resolve(url).func.view_class,
+            PostListView
+        )
+
+    def test_blog_post_detail_resolver(self):
+        url = reverse('blog:post-detail', kwargs={'post_id': 1})
+        self.assertEquals(
+            resolve(url).func.view_class,
+            PostDetailView
+        )
