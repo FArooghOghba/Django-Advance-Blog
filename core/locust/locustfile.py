7f467ab4d69bf38d739aaf05bd1bcffbf636ae46
FArooghOghba <faroogh.oghba@gmail.com>
2023-01-27 13:21:23 +0330

    build: load-test with locust
    install locust with docker-compose for load test.

---
+++ core/locust/locustfile.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+
+    def on_start(self):
+        response = self.client.post(
+            'accounts/api/v1/jwt/create/',
+            data={
+                'email': 'admin@website.com',
+                'password': 'far121269'
+            }
+        ).json()
+        self.client.headers = {
+            'Authorization': f"Bearer {response.get('access')}"
+        }
+
+    @task
+    def post_list(self):
+        self.client.get('blog/api/v1/post/')
+
+    @task
+    def category_list(self):
+        self.client.get('blog/api/v1/category/')
